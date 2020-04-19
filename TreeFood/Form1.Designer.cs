c50dca6127e8c7fca8a25cb5602b00d7f9336805
Andrey Schmider <shmider272244@mail.ru>
2020-04-19 20:33:47 +0600

    commit

---
+++ TreeFood/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace TreeFood
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Яблоки");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Бананы");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Фрукты", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2});
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Помидоры");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Овощи", new System.Windows.Forms.TreeNode[] {
+            treeNode4});
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Сыр");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Молочка", new System.Windows.Forms.TreeNode[] {
+            treeNode6});
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Белый хлеб");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Хлебобулочные изделия", new System.Windows.Forms.TreeNode[] {
+            treeNode8});
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Рыба");
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Мясо", new System.Windows.Forms.TreeNode[] {
+            treeNode10});
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Location = new System.Drawing.Point(12, 30);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Узел1";
+            treeNode1.Text = "Яблоки";
+            treeNode2.Name = "Узел3";
+            treeNode2.Text = "Бананы";
+            treeNode3.Name = "Узел0";
+            treeNode3.Text = "Фрукты";
+            treeNode4.Name = "Узел5";
+            treeNode4.Text = "Помидоры";
+            treeNode5.Name = "Узел2";
+            treeNode5.Text = "Овощи";
+            treeNode6.Name = "Узел8";
+            treeNode6.Text = "Сыр";
+            treeNode7.Name = "Узел7";
+            treeNode7.Text = "Молочка";
+            treeNode8.Name = "Узел11";
+            treeNode8.Text = "Белый хлеб";
+            treeNode9.Name = "Узел10";
+            treeNode9.Text = "Хлебобулочные изделия";
+            treeNode10.Name = "Узел14";
+            treeNode10.Text = "Рыба";
+            treeNode11.Name = "Узел12";
+            treeNode11.Text = "Мясо";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode5,
+            treeNode7,
+            treeNode9,
+            treeNode11});
+            this.treeView1.Size = new System.Drawing.Size(174, 205);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(203, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(203, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(203, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Записать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 247);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "TreeFood";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TreeFood/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using ServiceStack.Text;
+
+namespace TreeFood
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TreeNode node = new TreeNode(textBox1.Text);
+                treeView1.SelectedNode.Nodes.Add(node);
+            }
+            catch (Exception) { }
+
+        }
+
+        private void btnCreateTreeData()
+        {
+            // созданем буффер для хранения строки
+            System.Text.StringBuilder buffer = new System.Text.StringBuilder();
+            // цикл каждого узла дерева
+            foreach (TreeNode rootNode in treeView1.Nodes)
+                BuildTreeString(rootNode, buffer);
+            //запись
+            System.IO.File.WriteAllText(@"..\..\food.txt", buffer.ToString()); 
+
+        }
+        private void BuildTreeString(TreeNode rootNode, System.Text.StringBuilder buffer)
+        {
+            buffer.Append(rootNode.Text);
+            buffer.Append(Environment.NewLine);
+            foreach (TreeNode childNode in rootNode.Nodes)
+                BuildTreeString(childNode, buffer);
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RemoveCheckedNodes(treeView1.Nodes);
+
+        }
+
+        //удаление узлов
+        List<TreeNode> checkedNodes = new List<TreeNode>();
+        void RemoveCheckedNodes(TreeNodeCollection nodes)
+        {
+            foreach(TreeNode node in nodes)
+            { 
+                if (node.Checked)
+                {
+                    checkedNodes.Add(node);
+                }
+                else
+                {
+                    RemoveCheckedNodes(node.Nodes);
+                }
+            }
+            foreach (TreeNode checkedNode in checkedNodes)
+            {
+                nodes.Remove(checkedNode);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+            btnCreateTreeData();
+           
+        }
+    }
+}
